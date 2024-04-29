1c75bb22a0ab9273fb9f48c155a530d045bc7677
ChrisBotina7823 <1080691521@u.icesi.edu.co>
2024-04-28 23:59:33 -0500

    feat: implement distributed merging, sorting only allows multiples of block size

--- master/src/main/java/Master.java
+++ master/src/main/java/Master.java
@@ -7,17 +7,22 @@ import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class Master {
 
     public static final int BLOCK_SIZE = 100;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         try(Communicator communicator = Util.initialize(args)) {
             List<WorkerIPrx> workers = new ArrayList<>();
             Scanner scanner = new Scanner(System.in);
@@ -41,6 +46,7 @@ public class Master {
                 int fileIndex = 0;
                 Queue<WorkerIPrx> availableWorkers = new LinkedList<>(workers);
 
+                AtomicInteger sortingFilesCount = new AtomicInteger(0);
                 while((task = br.readLine()) != null) {
                     taskGroup.add(task);
                     // If the task group has 10 tasks, send it to a worker
@@ -54,6 +60,8 @@ public class Master {
                         CompletableFuture<String[]> futureArray = worker.doTaskAsync(taskGroupArray);
                         CompletableFuture<List<String>> future = futureArray.thenApply(Arrays::asList);
                         final int finalFileIndex = fileIndex;
+                        sortingFilesCount.incrementAndGet();
+                        System.out.println("Sending tasks to worker " + worker.ice_getIdentity().name + " for sorting" + finalFileIndex + ".txt");
                         future.thenAccept(sortedTasks -> {
                             // Write the sorted tasks to a temporary file
                             try(BufferedWriter bw = new BufferedWriter(new FileWriter("resources/temp" + finalFileIndex + ".txt"))) {
@@ -65,19 +73,85 @@ public class Master {
                                 e.printStackTrace();
                             }
                             availableWorkers.add(worker); // Add the worker back to the queue
+                            sortingFilesCount.decrementAndGet();
                         });
                         taskGroup.clear();
                         fileIndex++;
                     }
                 }
+                while(sortingFilesCount.get() > 0) {
+                    Thread.sleep(1000);
+                }
+                                
                 System.out.println("Tasks have been distributed to workers. Waiting for workers to finish...");
+            
+                System.out.println("Local Sorting completed.");
+                
+                // Merge files
+                Queue<String> fileQueue = new LinkedList<>();
+                for(int i = 0; i < fileIndex; i++) {
+                    fileQueue.add("resources/temp" + i + ".txt");
+                }
+                
+                while(fileQueue.size() > 1) {
+                    
+                    String file1 = fileQueue.poll();
+                    String file2 = fileQueue.poll();
+                    String newFile = "resources/temp" + fileIndex + ".txt";
+
+                    System.out.println("merging " + file1 + " and " + file2 + " into " + newFile);
+                
+                    // Wait for an available worker
+                    while(availableWorkers.isEmpty()) {
+                        Thread.sleep(1000);
+                    }
+                
+                    WorkerIPrx worker = availableWorkers.poll(); // Get the next available worker
+
+                    worker.mergeFilesAsync(file1, file2, newFile).thenAccept(result -> {
+                        availableWorkers.add(worker); // Add the worker back to the queue
+                        fileQueue.add(newFile); // Add the merged file back to the queue
+                    });
+                
+                    fileIndex++;
+                }
+                if(fileQueue.size() == 1) {
+                    String file1 = fileQueue.poll();
+                    String file2 = "resources/temp" + (fileIndex - 1) + ".txt";
+                    String newFile = "resources/temp" + fileIndex + ".txt";
+                
+                    System.out.println("merging " + file1 + " and " + file2 + " into " + newFile);
+                
+                    // Wait for an available worker
+                    while(availableWorkers.isEmpty()) {
+                        Thread.sleep(1000);
+                    }
+                
+                    WorkerIPrx worker = availableWorkers.poll(); // Get the next available worker
+                
+                    worker.mergeFilesAsync(file1, file2, newFile).thenAccept(result -> {
+                        availableWorkers.add(worker); // Add the worker back to the queue
+                        fileQueue.add(newFile); // Add the merged file back to the queue
+                    });                
+                }
+
+                for(int i=0; i<fileIndex; i++) {
+                    Path tempFilePath = Paths.get("resources/temp" + i + ".txt");
+                    try {
+                        Files.deleteIfExists(tempFilePath);
+                    } catch(IOException e) {
+                        System.out.println("Error deleting file: " + tempFilePath);
+                    }
+                }
+
+                Path sourcePath = Paths.get("resources/temp" + fileIndex + ".txt");
+                Path targetPath = Paths.get("resources/final.txt");
+                Files.move(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
+                
             } catch(IOException e) {
                 e.printStackTrace();
             }
 
-            // Wait for all tasks to complete
-            communicator.waitForShutdown();
-            System.out.println("Local Sorting completed.");
         }
     }
 }
--- worker/src/main/java/Worker.java
+++ worker/src/main/java/Worker.java
@@ -3,6 +3,13 @@ import com.zeroc.Ice.*;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class Worker implements WorkerI {
@@ -11,6 +18,8 @@ public class Worker implements WorkerI {
         Arrays.sort(tasks);
         return tasks;
     }
+
+
 
     public static void main(String[] args) {
         try(Communicator communicator = Util.initialize(args)) {
@@ -21,6 +30,40 @@ public class Worker implements WorkerI {
             ObjectPrx obj = adapter.add(new Worker(), Util.stringToIdentity(workerName));
             adapter.activate();
             communicator.waitForShutdown();
+        }
+    }
+
+
+
+    @Override
+    public void mergeFiles(String file1, String file2, String outputFile, Current current) {
+        try (
+            BufferedReader reader1 = new BufferedReader(new FileReader(file1));
+            BufferedReader reader2 = new BufferedReader(new FileReader(file2));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))
+        ) {
+            String line1 = reader1.readLine();
+            String line2 = reader2.readLine();
+    
+            while (line1 != null || line2 != null) {
+                if (line2 == null || (line1 != null && line1.compareTo(line2) <= 0)) {
+                    writer.write(line1);
+                    writer.newLine();
+                    line1 = reader1.readLine();
+                } else {
+                    writer.write(line2);
+                    writer.newLine();
+                    line2 = reader2.readLine();
+                }
+            }
+            try {
+                Files.deleteIfExists(Paths.get(file1));
+                Files.deleteIfExists(Paths.get(file2));
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
